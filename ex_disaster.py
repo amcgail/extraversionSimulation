a6241dcbd40487698f7a178af3e34cf489c7c64a
Alec McGail <amcgail2@gmail.com>
2017-10-18 23:35:56 -0400

    Update ex_disaster.py

--- ex_disaster.py
+++ ex_disaster.py
@@ -17,3 +17,46 @@ s.exportGraphCSV( "out" )
 s.exportDegreeDist( "out" )
 s.exportDegrees( "out" )
 s.exportUnhappinessEvolution( "out" )
+
+"""
+Step 50
+ -- t=2.53 / 25.00
+Step 100
+ -- t=6.31 / 25.00
+Step 150
+ -- t=15.84 / 25.00
+Person 28 killed
+Person 70 killed
+Person 21 killed
+Person 72 killed
+Person 6 killed
+Person 47 killed
+Person 51 killed
+Person 29 killed
+Person 55 killed
+Person 63 killed
+Person 0 killed
+Person 73 killed
+Person 3 killed
+Person 74 killed
+Person 12 killed
+Person 46 killed
+Person 66 killed
+Person 29 killed
+Person 10 killed
+Person 4 killed
+Person 55 killed
+Person 52 killed
+Person 8 killed
+Person 38 killed
+Person 29 killed
+Step 50
+ -- t=35.32 / 50.02
+Step 100
+ -- t=49.94 / 50.02
+Edges exported to out.edges.csv
+Nodes exported to out.nodes.csv
+Degree distribution exported to out.deg.csv
+All degrees for all users exported to out.deg.all.csv
+Unhappiness exported to out..unhap.csv
+"""
